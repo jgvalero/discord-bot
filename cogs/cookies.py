2ec3b15902e3e7f9faf0d085bb9221289ae98951
Jose Valero <jgvalero@proton.me>
2024-02-07 14:32:39 -0700

    Add blackjack

--- cogs/cookies.py
+++ cogs/cookies.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 
@@ -75,7 +76,9 @@ class Cookies(commands.Cog):
         """Mutes a user for 10 seconds! Costs 10 cookies!"""
         user_cookies = self.db.get_value(ctx.author.id, ctx.guild.id, "cookies")
         if user_cookies < 10:
-            await ctx.send(f"You don't have enough cookies ({ctx.author.display_name})!")
+            await ctx.send(
+                f"You don't have enough cookies ({ctx.author.display_name})!"
+            )
             return
         self.db.set_value(ctx.author.id, ctx.guild.id, "cookies", user_cookies - 10)
         await member.edit(mute=True)
@@ -90,7 +93,9 @@ class Cookies(commands.Cog):
         """Deafens a user for 10 seconds! Costs 10 cookies!"""
         user_cookies = self.db.get_value(ctx.author.id, ctx.guild.id, "cookies")
         if user_cookies < 10:
-            await ctx.send(f"You don't have enough cookies ({ctx.author.display_name})!")
+            await ctx.send(
+                f"You don't have enough cookies ({ctx.author.display_name})!"
+            )
             return
         self.db.set_value(ctx.author.id, ctx.guild.id, "cookies", user_cookies - 10)
         await member.edit(deafen=True)
@@ -120,14 +125,18 @@ class Cookies(commands.Cog):
 
         async def get_choice(player):
             def check(m, player):
-                return m.author == player and m.content in ["rock", "paper", "scissors"]
+                return m.author == player and m.content.lower in [
+                    "rock",
+                    "paper",
+                    "scissors",
+                ]
 
             try:
                 msg = await self.bot.wait_for(
                     "message", check=lambda m: check(m, player), timeout=60.0
                 )
-                await player.send(f"You chose {msg.content}! Good luck!")
-                return msg.content
+                await player.send(f"You chose {msg.content.lower}! Good luck!")
+                return msg.content.lower
             except asyncio.TimeoutError:
                 await ctx.send(f"{player.display_name} did not respond in time!")
                 return None
@@ -174,6 +183,81 @@ class Cookies(commands.Cog):
         await ctx.send(
             f"{ctx.author.display_name}'s stats:\nCookies: {cookies}\nMost valuable fish: {most_valuable_fish} cookies\nBaits: {baits}"
         )
+
+    @commands.command()
+    async def blackjack(self, ctx, wager: int):
+        """Starts a game of blackjack with a wager!"""
+
+        user_cookies = self.db.get_value(ctx.author.id, ctx.guild.id, "cookies")
+        if user_cookies < wager:
+            return await ctx.send(f"You don't have enough cookies to make this wager!")
+
+        DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+        DEALER_HIT_THRESHOLD = 17
+
+        random.shuffle(DECK)
+
+        player_hand = [DECK.pop(), DECK.pop()]
+        dealer_hand = [DECK.pop(), DECK.pop()]
+
+        await ctx.send(f"Your hand: {player_hand} ({sum(player_hand)})")
+        await ctx.send(f"Dealer's hand: {dealer_hand[0]} _")
+
+        while sum(player_hand) < 21:
+            await ctx.send("Would you like to hit or stand? [hit/stand]")
+
+            def check(m):
+                return m.author == ctx.author and m.content.lower() in ["hit", "stand"]
+
+            try:
+                message = await self.bot.wait_for("message", check=check, timeout=60.0)
+            except asyncio.TimeoutError:
+                await ctx.send(
+                    "Sorry, you took too long to respond! Because of that, you lose your cookies!"
+                )
+                self.db.set_value(
+                    ctx.author.id, ctx.guild.id, "cookies", user_cookies - wager
+                )
+                return
+
+            if message.content.lower() == "hit":
+                player_hand.append(DECK.pop())
+                await ctx.send(f"Your hand: {player_hand} ({sum(player_hand)})")
+            else:
+                break
+
+        if sum(player_hand) > 21:
+            await ctx.send("Bust! You lose!")
+            self.db.set_value(
+                ctx.author.id, ctx.guild.id, "cookies", user_cookies - wager
+            )
+        elif sum(player_hand) == 21:
+            await ctx.send("Blackjack! You win!")
+            self.db.set_value(
+                ctx.author.id, ctx.guild.id, "cookies", user_cookies + wager
+            )
+        else:
+            while sum(dealer_hand) < DEALER_HIT_THRESHOLD:
+                dealer_hand.append(DECK.pop())
+            await ctx.send(f"Dealer's hand: {dealer_hand} ({sum(dealer_hand)})")
+
+            if sum(dealer_hand) > 21:
+                await ctx.send("Dealer busts! You win!")
+                self.db.set_value(
+                    ctx.author.id, ctx.guild.id, "cookies", user_cookies + wager
+                )
+            elif sum(dealer_hand) < sum(player_hand):
+                await ctx.send("You win!")
+                self.db.set_value(
+                    ctx.author.id, ctx.guild.id, "cookies", user_cookies + wager
+                )
+            elif sum(dealer_hand) > sum(player_hand):
+                await ctx.send("You lose!")
+                self.db.set_value(
+                    ctx.author.id, ctx.guild.id, "cookies", user_cookies - wager
+                )
+            else:
+                await ctx.send("It's a tie! Zoo wee mama!")
 
 
 async def setup(bot):
