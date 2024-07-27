b47a5c60b192cbe731840207985f5d9b2bf12f80
Jose Valero <dev@josevalero.com>
2024-07-26 22:48:52 -0700

    Disabled fish to update command

--- cogs/fish.py
+++ cogs/fish.py
@@ -16,50 +16,51 @@ class Fish(commands.Cog):
         self.bot = bot
         self.db = Database("data/users.db")
 
-    @commands.command()
+    @app_commands.command()
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def fish(self, ctx, bait: str = None):
+    async def fish(self, interaction: discord.Interaction):
         """Fish! If you use bait, you're guaranteed to catch a fish."""
-        message_content = f"{ctx.author.display_name} is fishing"
+        # message_content = f"{ctx.author.display_name} is fishing"
 
-        if bait is not None and bait.lower() == "bait":
-            user_bait = self.db.get_value(ctx.author.id, ctx.guild.id, "bait")
-            if user_bait < 1:
-                return await ctx.send("You don't have any bait!")
-            else:
-                self.db.set_value(ctx.author.id, ctx.guild.id, "bait", user_bait - 1)
-                message_content += " (with bait)"
+        # if bait is not None and bait.lower() == "bait":
+        #     user_bait = self.db.get_value(ctx.author.id, ctx.guild.id, "bait")
+        #     if user_bait < 1:
+        #         return await ctx.send("You don't have any bait!")
+        #     else:
+        #         self.db.set_value(ctx.author.id, ctx.guild.id, "bait", user_bait - 1)
+        #         message_content += " (with bait)"
 
-        message = await ctx.send(message_content)
-        for i in range(7):
-            await asyncio.sleep(1)
-            message_content += "."
-            await message.edit(content=message_content)
-        await asyncio.sleep(1)
+        # message = await ctx.send(message_content)
+        # for i in range(7):
+        #     await asyncio.sleep(1)
+        #     message_content += "."
+        #     await message.edit(content=message_content)
+        # await asyncio.sleep(1)
 
-        if bait is not None and bait.lower() == "bait" or random.randint(1, 10) == 1:
-            fish_value = int((random.random() ** 2) * 100) + 1
-            user_cookies = self.db.get_value(ctx.author.id, ctx.guild.id, "cookies")
-            self.db.set_value(
-                ctx.author.id, ctx.guild.id, "cookies", user_cookies + fish_value
-            )
+        # if bait is not None and bait.lower() == "bait" or random.randint(1, 10) == 1:
+        #     fish_value = int((random.random() ** 2) * 100) + 1
+        #     user_cookies = self.db.get_value(ctx.author.id, ctx.guild.id, "cookies")
+        #     self.db.set_value(
+        #         ctx.author.id, ctx.guild.id, "cookies", user_cookies + fish_value
+        #     )
 
-            most_valuable_fish = self.db.get_value(
-                ctx.author.id, ctx.guild.id, "most_valuable_fish"
-            )
-            if fish_value > most_valuable_fish:
-                self.db.set_value(
-                    ctx.author.id, ctx.guild.id, "most_valuable_fish", fish_value
-                )
-                await ctx.send(
-                    f"{ctx.author.display_name} caught a fish worth {fish_value} cookies! This is your most valuable catch!"
-                )
-            else:
-                await ctx.send(
-                    f"{ctx.author.display_name} caught a fish worth {fish_value} cookies!"
-                )
-        else:
-            await ctx.send(f"Tough luck, {ctx.author.display_name}!")
+        #     most_valuable_fish = self.db.get_value(
+        #         ctx.author.id, ctx.guild.id, "most_valuable_fish"
+        #     )
+        #     if fish_value > most_valuable_fish:
+        #         self.db.set_value(
+        #             ctx.author.id, ctx.guild.id, "most_valuable_fish", fish_value
+        #         )
+        #         await ctx.send(
+        #             f"{ctx.author.display_name} caught a fish worth {fish_value} cookies! This is your most valuable catch!"
+        #         )
+        #     else:
+        #         await ctx.send(
+        #             f"{ctx.author.display_name} caught a fish worth {fish_value} cookies!"
+        #         )
+        # else:
+        #     await ctx.send(f"Tough luck, {ctx.author.display_name}!")
+        await interaction.response.send_message("There are no fish in the sea :(")
 
     @app_commands.command()
     async def bait(self, interaction: discord.Interaction, amount: int = 1):
