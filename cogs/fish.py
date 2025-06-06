bed5a94c0fdf48f89cd022e648d9acc4be4da222
Jose Valero <dev@josevalero.com>
2025-06-06 12:15:06 -0700

    Refactor fishing models to separate file

--- cogs/fish.py
+++ cogs/fish.py
@@ -1,105 +1,14 @@
 import random
-from typing import Dict, List, Literal, Optional
+from typing import Dict, Literal, Optional
 
 import discord
 import tomllib
 from discord import app_commands
 from discord.ext import commands
-from pydantic import BaseModel
 
 from main import DiscordBot
-from utils.database import Database
+from models.fishing import Bait, FishingSettings, FishingStats, Rod
 from utils.money import Money
-
-
-class Fish(BaseModel):
-    name: str
-    price: int
-    chance: float
-
-
-class Rod(BaseModel):
-    name: str
-    price: int
-    modifier: float
-
-
-class Bait(BaseModel):
-    name: str
-    price: int
-    modifier: float
-
-
-class FishingSettings(BaseModel):
-    base_catch_chance: float
-    fish: List[Fish]
-    rod: List[Rod]
-    bait: List[Bait]
-
-
-class FishingStats:
-    def __init__(self, user_id: int, guild_id: int, database: Database):
-        self.user_id: int = user_id
-        self.guild_id: int = guild_id
-        self.database: Database = database
-
-    @property
-    def total_fish_caught(self) -> int:
-        return self.database.get_value(
-            self.user_id, self.guild_id, "fishing", "total_fish_caught"
-        )[0]
-
-    @total_fish_caught.setter
-    def total_fish_caught(self, value) -> None:
-        self.database.set_value(
-            self.user_id, self.guild_id, "fishing", "total_fish_caught", value
-        )
-
-    @property
-    def total_weight(self) -> int:
-        return self.database.get_value(
-            self.user_id, self.guild_id, "fishing", "total_weight"
-        )[0]
-
-    @total_weight.setter
-    def total_weight(self, value) -> None:
-        self.database.set_value(
-            self.user_id, self.guild_id, "fishing", "total_weight", value
-        )
-
-    @property
-    def total_value(self) -> int:
-        return self.database.get_value(
-            self.user_id, self.guild_id, "fishing", "total_value"
-        )[0]
-
-    @total_value.setter
-    def total_value(self, value) -> None:
-        self.database.set_value(
-            self.user_id, self.guild_id, "fishing", "total_value", value
-        )
-
-    @property
-    def level(self) -> int:
-        return self.database.get_value(self.user_id, self.guild_id, "fishing", "level")[
-            0
-        ]
-
-    @level.setter
-    def level(self, value) -> None:
-        self.database.set_value(self.user_id, self.guild_id, "fishing", "level", value)
-
-    @property
-    def experience(self) -> int:
-        return self.database.get_value(
-            self.user_id, self.guild_id, "fishing", "experience"
-        )[0]
-
-    @experience.setter
-    def experience(self, value) -> None:
-        self.database.set_value(
-            self.user_id, self.guild_id, "fishing", "experience", value
-        )
 
 
 @app_commands.guild_only()
---
+++ models/fishing.py
@@ -0,0 +1,95 @@
+from typing import List
+
+from pydantic import BaseModel
+
+from utils.database import Database
+
+
+class Fish(BaseModel):
+    name: str
+    price: int
+    chance: float
+
+
+class Rod(BaseModel):
+    name: str
+    price: int
+    modifier: float
+
+
+class Bait(BaseModel):
+    name: str
+    price: int
+    modifier: float
+
+
+class FishingSettings(BaseModel):
+    base_catch_chance: float
+    fish: List[Fish]
+    rod: List[Rod]
+    bait: List[Bait]
+
+
+class FishingStats:
+    def __init__(self, user_id: int, guild_id: int, database: Database):
+        self.user_id: int = user_id
+        self.guild_id: int = guild_id
+        self.database: Database = database
+
+    @property
+    def total_fish_caught(self) -> int:
+        return self.database.get_value(
+            self.user_id, self.guild_id, "fishing", "total_fish_caught"
+        )[0]
+
+    @total_fish_caught.setter
+    def total_fish_caught(self, value) -> None:
+        self.database.set_value(
+            self.user_id, self.guild_id, "fishing", "total_fish_caught", value
+        )
+
+    @property
+    def total_weight(self) -> int:
+        return self.database.get_value(
+            self.user_id, self.guild_id, "fishing", "total_weight"
+        )[0]
+
+    @total_weight.setter
+    def total_weight(self, value) -> None:
+        self.database.set_value(
+            self.user_id, self.guild_id, "fishing", "total_weight", value
+        )
+
+    @property
+    def total_value(self) -> int:
+        return self.database.get_value(
+            self.user_id, self.guild_id, "fishing", "total_value"
+        )[0]
+
+    @total_value.setter
+    def total_value(self, value) -> None:
+        self.database.set_value(
+            self.user_id, self.guild_id, "fishing", "total_value", value
+        )
+
+    @property
+    def level(self) -> int:
+        return self.database.get_value(self.user_id, self.guild_id, "fishing", "level")[
+            0
+        ]
+
+    @level.setter
+    def level(self, value) -> None:
+        self.database.set_value(self.user_id, self.guild_id, "fishing", "level", value)
+
+    @property
+    def experience(self) -> int:
+        return self.database.get_value(
+            self.user_id, self.guild_id, "fishing", "experience"
+        )[0]
+
+    @experience.setter
+    def experience(self, value) -> None:
+        self.database.set_value(
+            self.user_id, self.guild_id, "fishing", "experience", value
+        )
